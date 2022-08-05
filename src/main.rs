7df981c313ca68b4568579c06e0e74f005d4c3b1
Ayodeji <ayodejisamuelfakunle@gmail.com>
2022-08-05 13:02:40 +0200

    Working on a simple library to help validate data especially when using Node.js

---
+++ src/main.rs
@@ -0,0 +1,165 @@
+use std::{collections::HashMap, error::Error};
+use serde::{Deserialize, Serialize};
+use serde_json::{Value, to_string};
+mod methods;
+
+
+
+
+type  HashmapData = HashMap<String, HashMap<String, String>>;
+
+
+
+#[derive(Serialize, Deserialize, Debug)]
+
+struct HashmapDataStruct {
+    content:  HashMap<String, HashmapData> 
+}
+struct DataTypes {
+    string:String,
+    number:String,
+    float:String,
+    email:String
+}
+
+enum EnumDataTypes {
+    String(String),
+    Number(i64),
+    Float(f32),
+    Email(String)
+}
+
+
+
+impl HashmapDataStruct {
+
+    fn new(data:HashMap<String, HashmapData>) ->  Self {
+        let  data = HashmapDataStruct {
+            content: data
+        };
+        data
+    }
+
+    fn loop_through(self) {
+        for i  in self.content.iter() {
+            println!("For I: {:?}", i.1);
+            HashmapDataStruct::validate_items(&i.1);
+        };
+    }
+    fn validate_items(items:&HashMap<String, HashMap<String, String>>) {
+        
+        match items["item"]["type"].as_str() {
+            "string" => println!("hey string"),
+            "number" => match items["item"]["value"].parse::<i64>().unwrap(){
+                _i64 => { 
+                    for method in items["methods"].iter() {
+                        HashmapDataStruct::validate_methods(method, &items["item"])
+                    }
+                },
+                _=>println!("{}", "bad value"),
+            },
+            "float" =>  match items["item"]["value"].parse::<f32>().unwrap(){
+                            _f32 => println!("{}", items["item"]["value"]),
+                            _=>println!("{}", "bad value"),
+                        },
+            "email" =>println!("hey email"),
+            "" => println!("hey unknown"),
+            _ =>println!("hey last option validate")
+        }
+ 
+    }
+    fn validate_methods((method, method_value):(&String, &String),  item: &HashMap<String, String>) {
+        match method.as_str() {
+            "required" =>{
+                            let result = require(&item["value"]);
+                            match_result(result, );
+                        }
+            "max" => {
+                        let result  = max(&item["value"], &method_value);
+                        match_result(result);
+                    },
+            "min"=>{
+                        let result  = min(&item["value"], &method_value);
+                        
+                        match_result(result);
+                    },
+            _=>println!("hey string what")
+        }
+        
+        fn match_result(res:Result<bool, bool>) {
+            match res {
+                Ok(true) => println!("Yay"),
+                Err(false) => println!("Nah"),
+                _=> println!("Nah"),
+            }
+        }
+        fn require(val: &str)-> Result<bool, bool>{
+          let result =   match val {
+               "" => Err(false),
+               _ => Ok(true)
+            };
+            result
+        }
+        fn max(val: &str, max:&str) -> Result<bool, bool>{
+            let val :i64 = val.len().try_into().unwrap();
+            let max = max.parse::<i64>().unwrap();
+            let result = if val <= max {
+                println!("{} {}", max, val);
+                Ok(true)
+            } else  {
+                Err(false)
+            };
+            result
+        }
+        fn min(val: &str, min:&str) -> Result<bool, bool>{
+            let val :i64 = val.len().try_into().unwrap();
+            let min = min.parse::<i64>().unwrap();
+            let result = if val <= min {
+                println!("{} {}", min, val);
+                Err(false)
+            } else  {
+                Ok(true)
+            };
+            result
+        }
+    
+    }
+
+
+    
+}
+
+fn main() {
+    let data1 = r#"
+    {
+        
+        "name":[
+                  {"item":{"name": "name","value": "John Doe", "type": "string"},  "methods":{"max": "100", "required": "true", "min":"10"}}
+                ],       
+        "password":[
+                    {"item":{"name": "password", "value": "1234567", "type": "number"},  "methods":{"max": "100", "required": "true", "min":"10"}}
+                  ]
+
+
+
+    
+    }"#;
+
+
+    let data = r#"
+    {
+        
+        "name":{"item":{"name": "name","value": "John Doe", "type": "string"},  "methods":{"max": "100", "required": "true", "min":"10"}},
+        "password":{"item":{"name": "password", "value": "1234567", "type": "number"},  "methods":{"max": "100", "required": "true", "min":"10"}},  
+        "phonenumber":{"item":{"name": "phonenumber", "value": "08133475878", "type": "number"},  "methods":{"max": "100", "required": "true", "min":"10"}}    
+
+
+
+
+    
+    }"#;
+
+
+    let res = HashmapDataStruct::new(serde_json::from_str(data).unwrap());
+    res.loop_through();
+}
---
+++ src/methods/methods.rs
@@ -0,0 +1,182 @@
+use std::collections::HashMap;
+
+struct Method<'a> {
+    method: &'static str,
+    method_value: &'static str,
+    items: &'a HashMap<String, HashMap<String, String>>
+}
+
+
+impl<'a> Method<'a> {
+
+    fn new((method, method_value):&(&'static str, &'static str), items:&'a HashMap<String, HashMap<String, String>>) -> Self{
+            let method_wrapper = Method {
+                method,
+                method_value,
+                items
+            };
+            method_wrapper
+    }
+
+    fn verify_method(&self) {
+        let item = &self.items["item"]["value"];
+        match self.method {
+            "required" =>{
+                            let result = Method::required(&item);
+                            Method::match_result(result);
+                        }
+            "max" => {
+                        let result  = Method::max(&item, self.method_value);
+                        Method::match_result(result);
+                    },
+            "min"=>{
+                        let result  = Method::min(&item, self.method_value);
+                        
+                        Method::match_result(result);
+                    },
+            _=>println!("hey string what")
+        }
+    }
+    fn match_result(res:Result<bool, bool>){
+        match res {
+            Ok(true) => println!("Yay"),
+            Err(false) => println!("Nah"),
+            _=> println!("Nah"),
+        }
+    }
+    fn required(val: &str)-> Result<bool, bool>{
+        let result =   match val {
+             "" => Err(false),
+             _ => Ok(true)
+          };
+          result
+    }
+    fn max(val: &str, max:&str) -> Result<bool, bool>{
+        let val :i64 = val.len().try_into().unwrap();
+        let max = max.parse::<i64>().unwrap();
+        let result = if val <= max {
+            println!("{} {}", max, val);
+            Ok(true)
+        } else  {
+            Err(false)
+        };
+        result
+    }
+    fn min(val: &str, min:&str) -> Result<bool, bool>{
+        let val :i64 = val.len().try_into().unwrap();
+        let min = min.parse::<i64>().unwrap();
+        let result = if val <= min {
+            println!("{} {}", min, val);
+            Err(false)
+        } else  {
+            Ok(true)
+        };
+        result
+    }
+}
+
+#[cfg(test)]
+mod method_test {
+    use  super::*;
+    #[test]
+    fn method() {
+       let mut items = HashMap::new();
+       let mut vals = HashMap::new();
+       vals.insert("name".to_string(), "name".to_string());
+       vals.insert("value".to_string(), "John Doe".to_string() );
+       vals.insert("type".to_string(), "string".to_string());
+       items.insert("Item".to_string(), vals);
+       let methods = ("max", "100");
+
+       let method_wrapper = Method::new(&methods, &items);
+        assert_eq!(method_wrapper.method_value, methods.1);
+    }   
+    #[test]
+    fn max_should_pass() {    
+        let result = Method::max("John Doe", "15");
+            let match_result = match result {
+                Ok(true) => true,
+                Err(false)=> false,
+                Ok(false)=> false,
+                Err(true) =>true,
+            };
+            
+
+        assert_eq!(match_result, true);
+    }
+    #[test]
+    fn max_should_fail() {
+        
+        let result = Method::max("John Doe", "5");
+            let match_result = match result {
+                Ok(true) => true,
+                Err(false)=> false,
+                Ok(false)=> false,
+                Err(true) =>true,
+            };
+            
+
+        assert_eq!(match_result, false);
+    }
+    #[test]
+    fn min_should_pass() {    
+        let result = Method::min("John Doe", "5");
+            let match_result = match result {
+                Ok(true) => true,
+                Err(false)=> false,
+                Ok(false)=> false,
+                Err(true) =>true,
+            };
+            
+
+        assert_eq!(match_result, true);
+    }
+    #[test]
+    fn min_should_fail() {
+        
+        let result = Method::min("John Doe", "10");
+            let match_result = match result {
+                Ok(true) => true,
+                Err(false)=> false,
+                Ok(false)=> false,
+                Err(true) =>true,
+            };
+            
+
+        assert_eq!(match_result, false);
+    }
+
+    
+    #[test]
+    fn required_should_pass() {
+        
+        let result = Method::required("John Doe");
+            let match_result = match result {
+                Ok(true) => true,
+                Err(false)=> false,
+                Ok(false)=> false,
+                Err(true) =>true,
+            };
+            
+
+        assert_eq!(match_result, true);
+    }
+    
+    #[test]
+    fn required_should_fail() {
+        
+        let result = Method::required("");
+            let match_result = match result {
+                Ok(true) => true,
+                Err(false)=> false,
+                Ok(false)=> false,
+                Err(true) =>true,
+            };
+            
+
+        assert_eq!(match_result, false);
+    }
+}
+
+
+
